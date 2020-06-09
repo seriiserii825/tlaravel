1843e1db4bd0ac3ade080a39f178d75ab47e23fd
serii <seriiburduja@gmail.com>
2020-06-09 15:38:17 +0300

    controller

---
+++ app/Http/Controllers/Admin/Core.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Core extends Controller
+{
+	public function getArticle($id){
+
+	}
+	public function getArticles() {
+		echo 'articles';
+	}
+}
