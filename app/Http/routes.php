a2efe60121f98ddec6b9ef5e1c128612f0d67d4c
serii <seriiburduja@gmail.com>
2020-06-09 13:07:58 +0300

    Route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,49 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', ['as' => 'home' , function () {
     return view('welcome');
+}]);
+//Route::get( '/page', function () {
+//	echo '<pre>';
+//	print_r($_ENV);
+//	echo '</pre>';
+
+//	Config::set( 'app.locale', 'ru' );
+//	echo Config::get('app.locale');
+//
+//	return;
+//} );
+
+//Route::post('/comments', function(){
+//	echo '<pre>';
+//	var_dump($_POST);
+//	echo '</pre>';
+//});
+//Route::match(['get', 'post'], '/comments', function () {
+//	echo '<pre>';
+//	var_dump( $_POST );
+//	echo '</pre>';
+//} );
+//Route::any( '/comments', function () {
+//	echo '<pre>';
+//	var_dump( $_POST );
+//	echo '</pre>';
+//} );
+//Route::get( '/page/{id?}', function ($id = 100) {
+//	var_dump($id);
+//} );
+//Route::get( '/page/{id?}', function ( $id = 100 ) {
+//	var_dump( $id );
+//} );
+
+Route::group(['prefix'=>'admin'], function(){
+	Route::get('page/create', function(){
+		echo 'page/create';
+		echo '<br><a href="'.route('home').'">Home</a>';
+	});
+	Route::get( 'page/edit', function () {
+		echo 'page/edit';
+		echo '<br><a href="' . route( 'home' ) . '">Home</a>';
+	} );
 });
-Route::get( '/page', function () {
-	return view( 'page' );
-} );
