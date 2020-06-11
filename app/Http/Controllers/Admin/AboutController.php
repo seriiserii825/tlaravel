8836381d5f28518c703ee2120f5acd2abc4442d9
serii <seriiburduja@gmail.com>
2020-06-11 15:01:31 +0300

    Db

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -10,6 +11,16 @@ use App\Http\Controllers\Controller;
 class AboutController extends Controller
 {
 	public function show() {
+		$articles = DB::select("SELECT * FROM `articles`");
+
+		dump($articles);
+
+//		DB::insert("INSERT INTO `articles` (`name`, `text`) VALUES (?, ?)", ['test1', 'text1']);
+
+//		$result = DB::update("UPDATE `articles` SET `name` = 'TEST 2' WHERE `id` = ?", [9]);
+
+		DB::delete("DELETE FROM `articles` WHERE `id` = ?", [10]);
+
 		return view( 'default.about', [ 'title' => 'Hello world' ] );
 	}
 }
