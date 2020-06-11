cb54c9d835974923ea9de8dd89b928bd5c6cfc2d
serii <seriiburduja@gmail.com>
2020-06-11 12:43:29 +0300

    migrations

---
+++ database/migrations/2020_06_11_093203_change_articles_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeArticlesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table( 'articles', function ( Blueprint $table ) {
+			$table->string( 'alias', 100 );
+		} );
+	}
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table( 'articles', function ( Blueprint $table ) {
+			$table->dropColumn( 'alias' );
+		} );
+	}
+}
