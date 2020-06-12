37a852524867b412580bd64297927b0906bb70b3
serii <seriiburduja@gmail.com>
2020-06-12 08:00:48 +0300

    query builder

--- app/Http/Controllers/Admin/Core.php
+++ app/Http/Controllers/Admin/Core.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use DB;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -13,6 +14,16 @@ class Core extends Controller
 
 	}
 	public function getArticles() {
-		echo 'articles';
+		// $articles = DB::table('articles')->get();
+		// $articles = DB::table('articles')->first();
+		// $articles = DB::table('articles')->pluck('name');
+		// $articles = DB::table('articles')->max('id');
+		// $articles = DB::table('articles')->select('id', 'name', 'text')->get();
+		// $articles = DB::table('articles')->distinct()->select('id', 'name', 'text')->get();
+		$articles = DB::table('articles')->distinct()->select('id', 'name', 'text')->where('id', '=', 2)->get();
+
+		echo '<pre>';
+		var_dump($articles);
+		echo '</pre>';
 	}
 }
