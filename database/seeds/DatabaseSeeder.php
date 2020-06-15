2a7ee83b764b16ca9e152db5f9cc0d0da3d5e185
serii <seriiburduja@gmail.com>
2020-06-15 09:13:43 +0300

    add seeders in DataBasesedder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,19 +2,20 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //  $this->call(CountriesTableSeeder::class);
-        //  $this->call(ArticlesSeeder::class);
-        //  $this->call(AboutPageSeeder::class);
-        //  $this->call(AddUserTableSeeder::class);
-         $this->call(StudentsTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		//  $this->call(CountriesTableSeeder::class);
+		//  $this->call(ArticlesSeeder::class);
+		//  $this->call(AboutPageSeeder::class);
+		//  $this->call(AddUserTableSeeder::class);
+		//         $this->call(StudentsTableSeeder::class);
+		$this->call( AddUserTableSeeder::class );
+		$this->call( RolesTableSeeder::class );
+		$this->call( RoleUserTableSeed::class );
+	}
 }
